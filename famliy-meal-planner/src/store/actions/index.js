b8feae38966fd773f8fd58f69e3bf8a62aea22d9
mparedes003 <onnabugeishadev@gmail.com>
2020-08-31 19:27:05 -0700

    Add Login Actions

--- famliy-meal-planner/src/store/actions/index.js
+++ famliy-meal-planner/src/store/actions/index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 // ACTION TYPES
 // one for fetching, one for fetched and one for errors
+
+// Sign Up Actions
 export const SIGN_UP_START = 'SIGN_UP_START';
 export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
 export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
@@ -26,6 +28,27 @@ export const signUp = (credentials, history) => (dispatch) => {
     })
     .catch((err) => {
       dispatch({ type: SIGN_UP_FAILURE, payload: err });
+      return false;
+    });
+};
+
+//Log In Actions
+export const LOGIN_START = 'LOGIN_START';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'LOGIN_FAILURE';
+
+export const logIn = (credentials, history) => (dispatch) => {
+  dispatch({ type: LOGIN_START });
+  axios
+    .post('http://localhost:9999/auth/login', credentials)
+    .then((res) => {
+      dispatch({ type: LOGIN_SUCCESS });
+      localStorage.setItem('token', res.data.token);
+      history.push('/');
+      return true;
+    })
+    .catch((err) => {
+      dispatch({ type: LOGIN_FAILURE, payload: err });
       return false;
     });
 };
