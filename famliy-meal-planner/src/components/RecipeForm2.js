5cd34db91a4c6214c6ad0575a4ac585377b39a6d
mparedes003 <onnabugeishadev@gmail.com>
2020-09-11 20:34:09 -0700

    First Stab at adding a recipe using react-final-form with field arrays. It is inputing the correct object with multiple ingredients and instructions but POST recipe does not work yet.

--- famliy-meal-planner/src/components/RecipeForm2.js
+++ famliy-meal-planner/src/components/RecipeForm2.js
@@ -1,77 +1,190 @@
 import React from 'react';
-import { Field, FieldArray, reduxForm } from 'redux-form';
+import { Form, Field } from 'react-final-form';
+import arrayMutators from 'final-form-arrays';
+import { FieldArray } from 'react-final-form-arrays';
+// import { addRecipe } from '../store/actions';
 
-const renderField = ({ input, label, type, meta: { touched, error } }) => (
-  <div>
-    <label>{label}</label>
-    <div>
-      <input {...input} type={type} placeholder={label} />
-      {touched && error && <span>{error}</span>}
-    </div>
-  </div>
-);
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-const renderIngredients = ({ fields, meta: { error, submitFailed } }) => (
-  <ul>
-    <li>
-      <button type='button' onClick={() => fields.push({})}>
-        Add Ingredient
-      </button>
-      {submitFailed && error && <span>{error}</span>}
-    </li>
-    {fields.map((ingredient, index) => (
-      <li key={index}>
-        <button
-          type='button'
-          title='Remove Ingredient'
-          onClick={() => fields.remove(index)}
-        />
-        <h4>Ingredient #{index + 1}</h4>
-        <Field
-          name={`${ingredient}.quantity`}
-          type='text'
-          component={renderField}
-          label='Quantity'
-        />
-        <Field
-          name={`${ingredient}.measurement`}
-          type='text'
-          component={renderField}
-          label='measurement'
-        />
-        <Field
-          name={`${ingredient}.name`}
-          type='text'
-          component={renderField}
-          label='name'
-        />
-      </li>
-    ))}
-  </ul>
-);
+const onSubmit = async (values) => {
+  await sleep(300);
+  window.alert(JSON.stringify(values, 0, 2));
+  // event.preventDefault();
+  // const newRecipe = values;
+  //   title: '',
+  //   ingredients: [
+  //     {
+  //       quantity: '',
+  //       measurement: '',
+  //       name: '',
+  //     },
+  //   ],
+  //   instructions: [
+  //     {
+  //       step_number: '',
+  //       description: '',
+  //     },
+  //   ],
+  // };
+  // console.log('newRecipe data', this.props.history);
+  // console.log('newRecipe data', newRecipe);
+  // addRecipe(JSON.stringify(values, 0, 2));
+};
 
-const RecipeForm2 = ({ handleSubmit, pristine, reset, submitting }) => {
-  return (
-    <form onSubmit={handleSubmit}>
-      <Field
-        name='title'
-        type='text'
-        component={renderField}
-        label='Recipe Name'
-      />
-      <FieldArray name='ingredients' component={renderIngredients} />
-      <div>
-        <button type='submit' disabled={submitting}>
-          Submit
-        </button>
-        <button type='button' disabled={pristine || submitting} onClick={reset}>
-          Clear Values
-        </button>
-      </div>
-    </form>
+const renderIngredientsArray = ({ fields }) => {
+  return fields.map(
+    (name, index) =>
+      console.log('field') || (
+        <div key={name}>
+          <label>Ingredient #{index + 1}</label>
+
+          <div>
+            {/* <RenderCount /> */}
+            <Field
+              name={`${name}.quantity`}
+              component='input'
+              placeholder='quantity'
+              // validate={validateFirstName}
+            />
+            <Field
+              name={`${name}.measurement`}
+              component='input'
+              placeholder='measurement'
+            />
+            <Field
+              name={`${name}.name`}
+              component='input'
+              placeholder='ingredient'
+            />
+            <span
+              role='img'
+              aria-label='Remove'
+              onClick={() => fields.remove(index)}
+              style={{ cursor: 'pointer' }}
+            >
+              ❌
+            </span>
+          </div>
+        </div>
+      )
   );
 };
 
-export default reduxForm({
-  form: 'RecipeForm2', // a unique identifier for this form
-})(RecipeForm2);
+const renderInstructionsArray = ({ fields }) => {
+  return fields.map(
+    (name, index) =>
+      console.log('field') || (
+        <div key={name}>
+          <label>Ingredient #{index + 1}</label>
+
+          <div>
+            {/* <RenderCount /> */}
+            <Field
+              name={`${name}.step_number`}
+              component='input'
+              placeholder='step number'
+              // validate={validateFirstName}
+            />
+            <Field
+              name={`${name}.description`}
+              component='input'
+              placeholder='description'
+            />
+            <span
+              role='img'
+              aria-label='Remove'
+              onClick={() => fields.remove(index)}
+              style={{ cursor: 'pointer' }}
+            >
+              ❌
+            </span>
+          </div>
+        </div>
+      )
+  );
+};
+
+const RecipeForm2 = () => (
+  <Form
+    onSubmit={onSubmit}
+    mutators={{
+      ...arrayMutators,
+    }}
+    subscription={{
+      submitting: true,
+    }}
+    initialValues={{
+      title: '',
+      ingredients: [],
+      instructions: [],
+    }}
+    render={({
+      handleSubmit,
+      form: {
+        mutators: { push, insert, pop },
+      }, // injected from final-form-arrays above
+      pristine,
+      form,
+      submitting,
+      values,
+    }) => {
+      return (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Title</label>
+
+            <Field name='title' component='input' />
+          </div>
+          <div className='buttons'>
+            <label>Ingredients</label>
+            <br />
+            <button
+              type='button'
+              onClick={() => push('ingredients', undefined)}
+            >
+              Add Ingredient
+            </button>
+            <button type='button' onClick={() => pop('ingredients')}>
+              Remove Ingredient
+            </button>
+          </div>
+          <FieldArray name='ingredients' subscription={{}}>
+            {renderIngredientsArray}
+          </FieldArray>
+          <div>
+            <label>Instructions</label>
+          </div>
+          <div className='buttons'>
+            <button
+              type='button'
+              onClick={() => push('instructions', undefined)}
+            >
+              Add Instruction
+            </button>
+            <button type='button' onClick={() => pop('instructions')}>
+              Remove Instruction
+            </button>
+          </div>
+          <FieldArray name='instructions' subscription={{}}>
+            {renderInstructionsArray}
+          </FieldArray>
+
+          <div className='buttons'>
+            <button type='submit' disabled={submitting || pristine}>
+              Submit
+            </button>
+            <button
+              type='button'
+              onClick={form.reset}
+              disabled={submitting || pristine}
+            >
+              Reset
+            </button>
+          </div>
+        </form>
+      );
+    }}
+  />
+);
+
+export default RecipeForm2;
