a5b75ea348e7fbe6cc080e22420bca8d8edf5e3c
mparedes003 <onnabugeishadev@gmail.com>
2020-09-17 16:05:15 -0700

    Cannot use action creator: Attempted to use react-final-form and react-final-form arrays to do dynamic inputs for inredients and instructions.

--- famliy-meal-planner/src/components/RecipeForm2.js
+++ famliy-meal-planner/src/components/RecipeForm2.js
@@ -1,108 +1,104 @@
 import React from 'react';
+// import { connect } from 'react-redux';
 import { Form, Field } from 'react-final-form';
 import arrayMutators from 'final-form-arrays';
 import { FieldArray } from 'react-final-form-arrays';
-// import { addRecipe } from '../store/actions';
+import { addRecipe } from '../store/actions';
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const onSubmit = async (values) => {
   await sleep(300);
-  window.alert(JSON.stringify(values, 0, 2));
-  // event.preventDefault();
-  // const newRecipe = values;
-  //   title: '',
-  //   ingredients: [
-  //     {
-  //       quantity: '',
-  //       measurement: '',
-  //       name: '',
-  //     },
-  //   ],
-  //   instructions: [
-  //     {
-  //       step_number: '',
-  //       description: '',
-  //     },
-  //   ],
-  // };
-  // console.log('newRecipe data', this.props.history);
-  // console.log('newRecipe data', newRecipe);
-  // addRecipe(JSON.stringify(values, 0, 2));
+  window.alert(JSON.stringify(values, null, 2));
+  const newRecipe = {
+    title: values.title,
+    ingredients: values.ingredients.map((ingredient) => ({ ...ingredient })),
+    instructions: values.instructions.map((instruction) => ({
+      ...instruction,
+    })),
+  };
+  // // console.log('newRecipe data', this.props.history);
+  console.log('newRecipe data', newRecipe);
+  this.props.addRecipe(newRecipe);
 };
 
-const renderIngredientsArray = ({ fields }) => {
-  return fields.map(
-    (name, index) =>
-      console.log('field') || (
-        <div key={name}>
-          <label>Ingredient #{index + 1}</label>
+// const renderIngredientsArray = ({ fields }) => {
+//   return fields.map(
+//     (name, index) =>
+//       console.log('field') || (
+//         <div key={name}>
+//           <label>Ingredient #{index + 1}</label>
 
-          <div>
-            {/* <RenderCount /> */}
-            <Field
-              name={`${name}.quantity`}
-              component='input'
-              placeholder='quantity'
-              // validate={validateFirstName}
-            />
-            <Field
-              name={`${name}.measurement`}
-              component='input'
-              placeholder='measurement'
-            />
-            <Field
-              name={`${name}.name`}
-              component='input'
-              placeholder='ingredient'
-            />
-            <span
-              role='img'
-              aria-label='Remove'
-              onClick={() => fields.remove(index)}
-              style={{ cursor: 'pointer' }}
-            >
-              ❌
-            </span>
-          </div>
-        </div>
-      )
-  );
-};
+//           <div>
+//             {/* <RenderCount /> */}
+//             <Field
+//               type='number'
+//               name={`${name}.quantity`}
+//               component='input'
+//               placeholder='quantity'
+//               // validate={validateFirstName}
+//             />
+//             <Field
+//               type='text'
+//               name={`${name}.measurement`}
+//               component='input'
+//               placeholder='measurement'
+//             />
+//             <Field
+//               type='text'
+//               name={`${name}.name`}
+//               component='input'
+//               placeholder='ingredient'
+//             />
+//             <span
+//               role='img'
+//               aria-label='Remove'
+//               onClick={() => fields.remove(index)}
+//               style={{ cursor: 'pointer' }}
+//             >
+//               ❌
+//             </span>
+//           </div>
+//         </div>
+//       )
+//   );
+// };
 
-const renderInstructionsArray = ({ fields }) => {
-  return fields.map(
-    (name, index) =>
-      console.log('field') || (
-        <div key={name}>
-          <label>Ingredient #{index + 1}</label>
+// const renderInstructionsArray = ({ fields }) => {
+//   return fields.map(
+//     (name, index) =>
+//       console.log('field') || (
+//         <div key={name}>
+//           <label>Ingredient #{index + 1}</label>
 
-          <div>
-            {/* <RenderCount /> */}
-            <Field
-              name={`${name}.step_number`}
-              component='input'
-              placeholder='step number'
-              // validate={validateFirstName}
-            />
-            <Field
-              name={`${name}.description`}
-              component='input'
-              placeholder='description'
-            />
-            <span
-              role='img'
-              aria-label='Remove'
-              onClick={() => fields.remove(index)}
-              style={{ cursor: 'pointer' }}
-            >
-              ❌
-            </span>
-          </div>
-        </div>
-      )
-  );
-};
+//           <div>
+//             {/* <RenderCount /> */}
+//             <Field
+//               type='number'
+//               name={`${name}.step_number`}
+//               component='input'
+//               placeholder='step number'
+//               // validate={validateFirstName}
+//             />
+//             <Field
+//               type='text'
+//               name={`${name}.description`}
+//               component='input'
+//               placeholder='description'
+//             />
+//             <span
+//               role='img'
+//               aria-label='Remove'
+//               onClick={() => fields.remove(index)}
+//               style={{ cursor: 'pointer' }}
+//             >
+//               ❌
+//             </span>
+//           </div>
+//         </div>
+//       )
+//   );
+// };
 
 const RecipeForm2 = () => (
   <Form
@@ -133,7 +129,17 @@ const RecipeForm2 = () => (
           <div>
             <label>Title</label>
 
-            <Field name='title' component='input' />
+            {/* <Field name='title' component='input' type='text' /> */}
+            <Field name='title'>
+              {(field) => (
+                <div className='input-row'>
+                  <input {...field.input} type='text' />
+                  {field.meta.touched && field.meta.error && (
+                    <span className='error'>{field.meta.error}</span>
+                  )}
+                </div>
+              )}
+            </Field>
           </div>
           <div className='buttons'>
             <label>Ingredients</label>
@@ -148,8 +154,50 @@ const RecipeForm2 = () => (
               Remove Ingredient
             </button>
           </div>
-          <FieldArray name='ingredients' subscription={{}}>
+          {/* <FieldArray name='ingredients' subscription={{}}>
             {renderIngredientsArray}
+          </FieldArray> */}
+          <FieldArray
+            name='ingredients'
+            // component={renderIngredients}
+            // subscription={{}}
+          >
+            {({ fields }) =>
+              fields.map(
+                (name, index) =>
+                  console.log('field') || (
+                    <div key={name}>
+                      <label>Ingredient #{index + 1}</label>
+
+                      <div>
+                        <Field
+                          name={`${name}.quantity`}
+                          component='input'
+                          placeholder='quantity'
+                        />
+                        <Field
+                          name={`${name}.measurement`}
+                          component='input'
+                          placeholder='measurement'
+                        />
+                        <Field
+                          name={`${name}.name`}
+                          component='input'
+                          placeholder='ingredient'
+                        />
+                        <span
+                          role='img'
+                          aria-label='Remove'
+                          onClick={() => fields.remove(index)}
+                          style={{ cursor: 'pointer' }}
+                        >
+                          ❌
+                        </span>
+                      </div>
+                    </div>
+                  )
+              )
+            }
           </FieldArray>
           <div>
             <label>Instructions</label>
@@ -165,8 +213,45 @@ const RecipeForm2 = () => (
               Remove Instruction
             </button>
           </div>
-          <FieldArray name='instructions' subscription={{}}>
+          {/* <FieldArray name='instructions' subscription={{}}>
             {renderInstructionsArray}
+          </FieldArray> */}
+          <FieldArray
+            name='instructions'
+            // component={renderInstructions}
+            // subscription={{}}
+          >
+            {({ fields }) =>
+              fields.map(
+                (name, index) =>
+                  console.log('field') || (
+                    <div key={name}>
+                      <label>Ingredient #{index + 1}</label>
+
+                      <div>
+                        <Field
+                          name={`${name}.step_number`}
+                          component='input'
+                          placeholder='step number'
+                        />
+                        <Field
+                          name={`${name}.description`}
+                          component='input'
+                          placeholder='description'
+                        />
+                        <span
+                          role='img'
+                          aria-label='Remove'
+                          onClick={() => fields.remove(index)}
+                          style={{ cursor: 'pointer' }}
+                        >
+                          ❌
+                        </span>
+                      </div>
+                    </div>
+                  )
+              )
+            }
           </FieldArray>
 
           <div className='buttons'>
